cb1abea0fb46635d518a4b1cf3b0913077b7ad90
kobe <kobenorcross@gmail.com>
2021-03-07 00:09:52 -0800

    updated octangle function, problems are rising

--- StarQuilt.py
+++ StarQuilt.py
@@ -1,7 +1,7 @@
 # setup
 import turtle as t
 res = 500                                       # resolution (x,y of the window)
-sideLength = 100                                # length of sides 
+sideLength = 50                                # length of sides 
 depth = 5
 t.setup(res, res)                         
 t.setworldcoordinates(0, 9, res, res)
@@ -12,18 +12,21 @@ t.goto(res/2, res/2)
 
 
 
-def triangle(length):
+def triangle(length, color):
     t.pd()
     #t.color("blue")
     #t.begin_fill()
     for i in range(0,3):
         t.fd(length)
         t.rt(360/3)
+    siepernski(length, depth, color)
+    
     #t.end_fill()
 
 
 
-def siepernski(length, depth):
+def siepernski(length, depth, color):
+    t.color(color)
     if (depth == 0):
         t.pd()
         t.stamp()
@@ -33,40 +36,40 @@ def siepernski(length, depth):
         t.pu()
         for i in range(0, 3):
             t.fd(length)
-            siepernski(length/2, depth-1)
+            siepernski(length/2, depth-1, color)
             t.backward(length)
             t.lt(120)
             
 
 
-def octagon(length):
+def octagon(length, colorOne, colorTwo):
     t.pu()
-    t.goto(9*res/4, res/3)
+    t.goto(res/2, res/2)
     t.fd(res/4)
-    t.rt(20)
-
+    #t.rt(20)
     t.pd()
-    t.begin_fill()
+    #t.begin_fill()
     for i in range(0, 8):
         t.lt(90)
-        #siepernski(length, depth)
-        triangle(length)
+        siepernski(length, depth, colorTwo)
+        #triangle(length)
         t.fd(length)
         t.rt(360/8)
-    t.end_fill()
+    #t.end_fill()
 
 
 
-def octagonInnerDesign(length):
+def octagonInnerDesign(length, color):
+    t.color(color)
     t.pu()
-    t.goto(5*res/2, res/2)
+    t.goto(res/2, res/2)
     t.fd(res/4)
     t.rt(20)
     t.pd()
     #t.begin_fill()
     for i in range(0, 8):
         t.rt(180)
-        triangle(length)
+        triangle(length, color)
         t.fd(length)
         t.rt(360/8)
     #t.end_fill()
@@ -74,8 +77,11 @@ def octagonInnerDesign(length):
 
 
 #build our star quilt:
-#octagonInnerDesign(sideLength)
-octagon(sideLength)
-#triangle(sideLength)
+octagon(sideLength, "light blue", "pink")
+#octagonInnerDesign(sideLength/2, "red")
+
+t.goto(res/2, res/2)
+t.lt(90)
 #siepernski(sideLength, depth)
+#triangle(sideLength, color)
 t.done()
