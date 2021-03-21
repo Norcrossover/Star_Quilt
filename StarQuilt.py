e79d3fd68cd708e0319f67316eb1fce4fa520043
Kobe <kobenorcross@gmail.com>
2021-03-20 19:25:59 -0700

    Created inner design and alt colors for triangle

--- StarQuilt.py
+++ StarQuilt.py
@@ -4,12 +4,12 @@ res = 500                                       # resolution (x,y of the window)
 sideLength = res/5                                # length of sides 
 depth = 5
 sides = 6
+colorOne = "black"
+colorTwo = "white"
 t.setup(res, res)                         
 t.setworldcoordinates(0, 9, res, res)
-'''
 t.tracer(0, 100)
 t.speed(0)
-'''
 t.pu()
 t.goto(res/2, res/2)
 
@@ -23,33 +23,39 @@ def octagon(sides, length, colorOne, colorTwo):
     t.lt(90)
     t.fd(res/4)
     t.pd()
+    t.color(colorTwo)
+    t.begin_fill()
     for i in range(0, sides):
         t.color(colorOne)
-        triangle(length, colorOne)
+        triangle(length, colorOne, colorTwo)
         t.rt(360/6)
         t.fd(length)
+    t.end_fill()
     t.fd(length/2)
     t.pu()
+    t.goto(res/2,res/2)
+    #innerDesign(8, length/2+14, colorOne, point)
     
 
 
 
-def triangle(length, color):
-    
-    t.pd()
-    theta = 0
-    t.lt(theta)
+def triangle(length, colorOne, colorTwo):
+    # setup variables needed
     a = (0, 0)
     b = (0, 0)
     c = (0, 0)
-    o = (0, 0)
-    ox = 0
-    oy = 0
     heading1 = 0
     heading2 = 0
     heading3 = 0
+    o = (0, 0)
+    ox = 0
+    oy = 0
+    t.pd()
+    t.color(colorOne)
+    t.begin_fill()
     for i in range(0,3):
-        #need to get the point and heading when the triangle function is called
+        t.pd()
+        # need to get the point and heading when the triangle function is called
         if (i == 0):
             a = t.pos()
             heading1 = t.heading()
@@ -57,42 +63,27 @@ def triangle(length, color):
             b = t.pos()
             heading2 = t.heading()
         elif (i == 2):
+            t.pu()
             c = t.pos()
             heading3 = t.heading()
+        #move forward length 
         t.fd(length)
+        #then right turn 
         t.rt(360/3)
-
-    # find the center of the triangle each time
+    t.end_fill()
+    #use the formula to find the x and y coordinate of o
     ox = (a[0]+b[0]+c[0])/3
     oy = (a[1]+b[1]+c[1])/3
     o = (ox, oy)
-    '''
-    print("Point 1:")
-    print(a)
-    print(heading1)
-    print("Point 2:")
-    print(b)
-    print(heading2)
-    print("Point 3:")
-    print(c)
-    print(heading3)
-    '''
-    #t.goto(point)
-    #t.seth(heading) 
     t.rt(90)
-    #need to go to the center of the triangle b having the point x and y = 0
     t.pu()
-    #add 5 to point 1 if the outcome is not as desired
     t.goto(o)
     t.pd()
-    siepernski(length/4, 3, color)
+    #draw the siepernski triangle
+    siepernski(length/4+7, 3, colorTwo)
     t.pu()
     t.fd(length/2)
-    t.rt(90)
-    # one corner of the serpinski triangle is aequal to the actual triangle i want
-    # go to the first points positiion ((x + length), 0)
-    t.rt(theta)
-    
+    t.rt(90)    
     t.goto(a)
     t.seth(heading1)    
     
@@ -101,6 +92,7 @@ def triangle(length, color):
 
  
 def siepernski(length, depth, color):
+    # draw the triangle with the given color
     t.color(color)
     if (depth == 0):
         t.pd()
@@ -117,31 +109,27 @@ def siepernski(length, depth, color):
 
 
 
-def innerDesign(sides, length, color):
+def innerDesign(sides, length, colorOne, colorTwo, startingPoint):
     t.color(color)
     t.pu()
-    t.goto(res/2, res/2)
-    t.fd(res/4)
-    t.rt(20)
+    t.goto(res/2-4,res/2+50)
+    t.fd(length)
+    t.rt(90)
     t.pd()
-    #t.begin_fill()
+    t.begin_fill()
     for i in range(0, sides):
-        t.rt(180)
-        '''
+        t.rt(360/sides)
         for j in range(0,3):
             t.fd(length)
             t.rt(360/sides)
-            '''
         t.fd(length)
         t.rt(360/sides)
-    #t.end_fill()
+    t.end_fill()
 
 
 
 #build our star quilt:
-octagon(sides, sideLength, "black", "white")
-#innerDesign(8, sideLength, "black")
-t.goto(res/2, res/2)
-t.rt(60)
-#triangle(sideLength, "black")
+#octagon(sides, sideLength, "blue", "pink")
+#triangle(sideLength, "black", "white")
+innerDesign(8, sideLength/2, colorOne, colorTwo, (res/2,res/2))
 t.done()
